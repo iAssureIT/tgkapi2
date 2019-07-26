7b7559266f3f60bec99ecb35e7d9945df773cecd
Omkar R <omkar@Omkars-Air.Dlink>
2019-07-26 13:22:59 +0530

    Omkar change in images

--- api/coreAdmin/routes/projectSettings.js
+++ api/coreAdmin/routes/projectSettings.js
@@ -3,9 +3,13 @@ const router 	= express.Router();
 
 const projectsettingController = require('../controllers/projectSetting');
 
+router.post('/', projectsettingController.create_projectSettings);
+
 router.get('/get/one/:type', projectsettingController.fetch_projectsettings);
 
-router.post('/', projectsettingController.create_projectSettings);
+router.get('/list', projectsettingController.list_projectsettings);
+
+
 
 
 module.exports = router;
