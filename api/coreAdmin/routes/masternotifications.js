991c314400f871ad7435ced21ae3479c7d22c29a
Omkar R <omkar@Omkars-Air.Dlink>
2019-08-08 16:54:12 +0530

    Omkar change in app

--- api/coreAdmin/routes/masternotifications.js
+++ api/coreAdmin/routes/masternotifications.js
@@ -25,7 +25,7 @@ router.get('/:notificationmasterID', masterNotificationsController.detail_fetch)
 
 router.put('/:notificationmasterID', masterNotificationsController.update_notifications);
 
-router.get('/get/sendemailnotification', masterNotificationsController.userEmalId_byId_fornotification);
+// router.get('/get/sendemailnotification', masterNotificationsController.userEmalId_byId_fornotification);
 
 
 
