5c8343e02889911bd2c5f0a55f94b429c5fd07cc
ashishnaik021 <iassureit@gmail.com>
2019-08-13 09:22:31 +0530

    Ashish Changes

--- api/TGK/controllers/properties.js
+++ api/TGK/controllers/properties.js
@@ -12,7 +12,7 @@ exports.create_Properties = (req,res,next)=>{
 
     async function main(){
         var allocatedToUserId = await getAllocatedToUserID(); 
-        var propertyCode = data.length + 101;
+        var propertyCode = 101; // data.length + 101;
         const properties = new Properties({
                 _id                     : new mongoose.Types.ObjectId(),
                 owner_id                : req.body.uid,
