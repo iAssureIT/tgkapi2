53c36b4f34738191ece07a43ab634a6a67c8636e
Rushikesh Salunkhe <rushikeshsalunkhe101@gmail.com>
2019-09-09 12:13:53 +0530

    Rushikesh change post property ui

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http 				= require('http');
 const app 				= require('./app'); // app file include
 const globalVariable 	= require('./nodemon.js');
 const port = process.env.PORT || globalVariable.port;
+console.log("port=>",port)
 
 const server = http.createServer(app);
 
