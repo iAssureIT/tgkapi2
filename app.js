cfdd389df25b5f3710316109ba866676584f4dd7
angelasmi <asmi3688@gmail.com>
2019-10-21 16:44:15 +0530

    Asmita pulled code

--- app.js
+++ app.js
@@ -113,6 +113,7 @@
 	const interestedPropertiesurl 	= require("./api/TGK/routes/interestedProperties");
     const searchPropertiesurl       = require("./api/TGK/routes/searchProperties");
     const salesAgenturl             = require("./api/TGK/routes/salesagent");
+    const fieldAgenturl             = require("./api/TGK/routes/fieldagent");
 	
 	app.use("/api/properties",propertiesurl);
 	app.use("/api/sellometers",sellometeorurl);
@@ -121,6 +122,7 @@
 	app.use("/api/interestedProperties",interestedPropertiesurl);
 	app.use("/api/search",searchPropertiesurl);
 	app.use("/api/salesagent",salesAgenturl);
+	app.use("/api/fieldagent",fieldAgenturl);
 
 
 	app.use((req, res, next) => {
