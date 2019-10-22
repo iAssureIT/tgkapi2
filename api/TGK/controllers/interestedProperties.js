dfcf68560f6630e243c93344b8f1bc566e335023
anaghakomawar <madhavir1982@gmail.com>
2019-10-22 17:12:05 +0530

    Anagha

--- api/TGK/controllers/interestedProperties.js
+++ api/TGK/controllers/interestedProperties.js
@@ -4,29 +4,131 @@ const InterestedProps = require('../models/interestedProperties');
 const Properties = require('../models/properties');
 const ObjectId = require('mongodb').ObjectID;
 
-
-exports.create_interestedProps = (req,res,next)=>{ 
-    const interestedProps = new InterestedProps({
-            _id                     : new mongoose.Types.ObjectId(),
-            buyer_id                : req.body.buyer_id,
-            property_id             : req.body.property_id,
-            createdAt               : new Date()
+function allocateTofieldAgent(propertyID)=>{
+    return new Promise(function(resolve,reject){
+        Properties.findOne({"_id" : ObjectID(propertyID)})
+                  .exec()
+                  .then(property=>{
+                    if(property){
+                        console.log("property _id",property.propertyLocation.pincode);
+                        CompanySettings.findOne(
+                                                {
+                                                    "companyId"                             : 1,
+                                                    "companyLocationsInfo.pincodesCovered"  : property.propertyLocation.pincode
+                                                },
+                                                {
+                                                    "_id"                  : 0, 
+                                                    "companyLocationsInfo" : {
+                                                            $elemMatch: {"pincodesCovered" : property.propertyLocation.pincode}
+                                                        }
+                                                }
+                                            )
+                                        .exec()
+                                        .then(csdata=>{
+                                            Users.find({"roles" : "Field Agent","officeLocation" : ObjectID(csdata.companyLocationsInfo[0]._id) })
+                                                 .sort({updateAt:1})
+                                                 .exec()
+                                                 .then(fieldAgents=>{
+                                                    console.log("fieldAgents ",fieldAgents);
+                                                    if(fieldAgents.length > 0){
+                                                        //Sales agents found. Then find, to which SA, the last property was assigned
+                                                        Users.updateOne(
+                                                                    { "_id" : fieldAgents[0]._id},
+                                                                    {
+                                                                        $set : {
+                                                                            "updateAt"              : new Date(),
+                                                                            "profile.propertyCount" : fieldAgents[0].profile.propertyCount ? salesAgents[0].profile.propertyCount + 1 : 1
+                                                                        }
+                                                                    }
+                                                                )
+                                                             .exec()
+                                                             .then(data=>{
+                                                                resolve(fieldAgents[0]._id)
+                                                             })
+                                                             .catch(err =>{
+                                                                res.status(500).json({
+                                                                    error: err
+                                                                   });
+                                                               });      
+                                                    }else{
+                                                        Users.findOne({"roles" : "Field Manager"})
+                                                            .exec()
+                                                            .then(fieldAgents=>{
+                                                                resolve(fieldAgents[0]._id);
+                                                            })
+                                                           .catch(err =>{
+                                                            res.status(500).json({
+                                                                message : "Admin role user Not Found",
+                                                                error: err
+                                                               });
+                                                           });
+                                                    }
+                                                 })
+                                                .catch(err =>{
+                                                  console.log(err);
+                                                    Users.findOne({"roles" : "Field Manager"})
+                                                    .exec()
+                                                    .then(fieldAgents=>{
+                                                        resolve(fieldAgents[0]._id);
+                                                    })
+                                                   .catch(err =>{
+                                                    res.status(500).json({
+                                                        message : "Admin role user Not Found",
+                                                        error: err
+                                                       });
+                                                   });
+                                                });
+                                        })
+                                        .catch(err =>{
+                                                console.log(err);
+                                                res.status(500).json({
+                                                    error: err
+                                                });
+                                            });
+                    }else{
+                        reject({message:"Property Not Found"});
+                    }
+                  })
+                  .catch(err =>{
+                        console.log(err);
+                        reject(err);
+                    })
     });
 
-    interestedProps.save()
-        .then(data=>{        
-            res.status(200).json({
-                "message" : 'Property Interest from this Buyer is Saved',
-            });
-        })
-        .catch(err =>{
-            console.log(err);
-            res.status(500).json({
-                message : "Some error in saving the Interested Buyer",
-                error: err
-            });
+};
+
+exports.create_interestedProps = (req,res,next)=>{ 
+    main();
+    async function main(){
+        var fieldAgent = await allocateTofieldAgent(req.body.property_id); 
+        const interestedProps = new InterestedProps({
+                _id                     : new mongoose.Types.ObjectId(),
+                buyer_id                : req.body.buyer_id,
+                property_id             : req.body.property_id,
+                createdAt               : new Date(),
+                fieldAgent              : [{
+                                               agentID    :  fieldAgent,
+                                               createdAt  : new Date(),
+                                               status     : "Active",
+                                               remark     : "New Interested Property"
+                                           }]
         });
-		           
+
+        interestedProps.save()
+            .then(data=>{        
+                res.status(200).json({
+                    "message" : 'Property Interest from this Buyer is Saved',
+                });
+            })
+            .catch(err =>{
+                console.log(err);
+                res.status(500).json({
+                    message : "Some error in saving the Interested Buyer",
+                    error: err
+                });
+            });
+    		           
+    }
 };
 
 exports.detail_interestedProps = (req, res, next)=>{
--- api/TGK/models/interestedProperties.js
+++ api/TGK/models/interestedProperties.js
@@ -5,6 +5,33 @@ const interestedPropsSchema = mongoose.Schema({
     buyer_id                : { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
     property_id             : { type: mongoose.Schema.Types.ObjectId, ref: 'properties' },
     createdAt               : Date,
+    fieldAgent              : [{
+                                   agentID    :  { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+                                   createdAt  : Date,
+                                   status     : String, //"Active" or "Inactive"
+                                   remark 	  : String
+                               }],
+    meeting 				: [{
+    								meetingDate : Date,
+    								remark 		: String,
+    							}],
+    query 					: [{
+    								remark 		: String,
+    								createdAt	: Date,
+    								agentRole	: { type: mongoose.Schema.Types.ObjectId, ref: 'roles' },
+    								agentID 	: { type: mongoose.Schema.Types.ObjectId, ref: 'users' }
+    							}],
+    status 					: String, // "Interested" , Meeting , Shown , Shortlisted, Token_Received, Contract_Due, Cancelled, Completed 
+    token_received 			: {
+    								amount 		: Number,
+    								receivedAt  : Date,
+    								remark		: String, 
+    							},
+    Contract_Due 			: {
+    								dueDate : String,
+    								remark  : String,
+    							}
+
    
 });
 
