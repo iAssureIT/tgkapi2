33ad5dbfc8dec77ec88aaf958f340a7a6bb17a7d
anaghakomawar <madhavir1982@gmail.com>
2019-11-04 11:29:25 +0530

    Anagha

--- api/TGK/controllers/properties.js
+++ api/TGK/controllers/properties.js
@@ -654,7 +654,7 @@ exports.list_Properties_salesAgent_type = (req,res,next)=>{
                     "salesAgent.agentID" : ObjectID(req.params.salesAgentID),
                     "salesAgent.status"  : "Active",
                     "status"                : req.params.status,
-                    "createdAt"             : {$ne : moment(new Date()).formate("YYYY-MM-DD")}
+                    "createdAt"             : {$ne : moment(new Date()).format("YYYY-MM-DD")}
                 };
     }
     console.log("query ",query);
