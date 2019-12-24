0b7c40365c23290e89e2a672b11706c82549b409
Rushikesh Salunkhe <rushikeshsalunkhe101@gmail.com>
2019-12-24 18:38:36 +0530

    Rushikesh changed in societies unapproved list

--- api/coreAdmin/controllers/otp.js
+++ api/coreAdmin/controllers/otp.js
@@ -123,6 +123,108 @@ exports.users_verify_mobile = (req,res,next)=>{
                             mobile   : req.body.mobileNumber,
                             // userId   : mongoose.Types.ObjectId(user._id) ,
                             userId  : user._id ,
+                        },globalVariable.JWT_KEY,
+                        {
+                            expiresIn: "365d"
+                        }
+                        );
+                    // console.log("otp ",OTP);
+                        User.updateOne(
+                                    { 'mobileNumber':req.body.mobileNumber, 'countryCode' : req.body.countryCode},
+                                    {
+                                        // $set : { "otp" : OTP},
+                                        $push : {
+                                            "services.resume.loginTokens" : {
+                                                    when: new Date(),
+                                                    hashedToken : token
+                                                }
+                                        },
+                                    }
+                                )
+                                .exec()
+                                .then(updateUser=>{
+                                    if(updateUser.nModified == 1){
+                                        setotp();
+                                        async function setotp(){
+                                            var uotp = await updateOTP(user._id,OTP);
+                                            if(uotp){
+                                                console.log("token=>",token);
+                                                res.status(200).json({
+                                                    "message"           : msg,
+                                                    "user_id"           : user._id,
+                                                    "otp"               : OTP,
+                                                    "count"             : 1,
+                                                    "fullName"          : user.profile.fullName ? user.profile.fullName : "",
+                                                    "token"               : token,
+                                                    "userProfileImg"      : user.profile.userProfile,
+                                                }); 
+                                            }else{
+                                                res.status(200).json({
+                                                    "message"           : msg + 'OTP Not Updated',
+                                                    "user_id"           : user._id,
+                                                    "otp"               : OTP,
+                                                    "count"             : 1,
+                                                    "fullName"          : user.profile.fullName ? user.profile.fullName : "",
+                                                    'token'             : token,
+                                                    "userProfileImg"    : user.profile.userProfile,
+                                                });
+                                            }
+                                        }
+                                    }
+                                })
+                                .catch(err=>{
+                                    console.log("500 err ",err);
+                                    res.status(500).json(err);
+                                });         
+                // })
+                // .catch(otpError=>{
+                //     return res.status(501).json({
+                //         message: "Some Error Occurred in OTP Send Function",
+                //         error: otpError
+                //     });        
+                // });
+            }
+
+        })
+        .catch(err =>{
+            console.log(err);
+            res.status(200).json({
+                message:"MOBILE-NUMBER-NOT-FOUND", 
+                error: err,
+            });
+        });
+};
+
+exports.users_verify_mobile_new = (req,res,next)=>{
+    // console.log("body data ",req.body);
+    User.findOne({'mobileNumber':req.body.mobileNumber, 'countryCode' : req.body.countryCode},{'profile.fullName':1})
+        .exec()
+        .then(user =>{
+            var userData = user;
+            // console.log('1. USER = ',user);
+            getData();
+            async function getData(){
+                var msg = "MOBILE-NUMBER-EXISTS";
+                if(!user){
+                    var newUser = await createUser(req.body.mobileNumber,req.body.countryCode);
+                    user = newUser;
+                    msg = "NEW-USER-CREATED";
+                }
+                // const OTP = getRandomInt(1000,9999);
+                const OTP = 1234;
+                // const client = new plivo.Client('MAMZU2MWNHNGYWY2I2MZ', 'MWM1MDc4NzVkYzA0ZmE0NzRjMzU2ZTRkNTRjOTcz');
+                // const sourceMobile = "+919923393733";
+                // var text = OTP+" is your OTP for online verification to your LYVO Account. OTP is valid for 24 hours and can be only used once."; 
+                // client.messages.create(
+                //     src=sourceMobile,
+                //     dst=req.body.countryCode+''+req.body.mobileNumber,
+                //     text=text
+                // ).then((result)=> {
+                    // console.log("src = ",src," | DST = ", dst, " | result = ", result);
+                    const token = jwt.sign({
+                            mobile   : req.body.mobileNumber,
+                            // userId   : mongoose.Types.ObjectId(user._id) ,
+                            userId  : user._id ,
                         },"secret",
                         {
                             expiresIn: "24h"
@@ -189,7 +291,7 @@ exports.users_verify_mobile = (req,res,next)=>{
 };
 
 
-exports.verify_user = (req,res,next)=>{
+exports.verify_user_new = (req,res,next)=>{
     console.log("body data ",req.body);
     User.findOne({_id:req.body.userId},{'profile.fullName':1,'profile.emailId':1,"profile.otp":1,"profile.mobileNumber":1,"services.resume.loginTokens":1})
         .exec()
--- api/coreAdmin/routes/otpBasedLogin.js
+++ api/coreAdmin/routes/otpBasedLogin.js
@@ -10,10 +10,13 @@ const checkAuth     = require('../middlerware/check-auth');
 
 
 const OTPController = require('../controllers/otp');
-
+//Old API
 router.post('/verify_mobile', OTPController.users_verify_mobile); 
-router.post('/verifyuser', OTPController.verify_user); 
+// router.post('/verifyuser', OTPController.verify_user); 
 
+//New API
+router.post('/verify_mobile_new', OTPController.users_verify_mobile_new); 
+router.post('/verify_user_new', OTPController.verify_user_new); 
 // router.post('/otpverified', OTPController.login_otp_verifications);
 
 
